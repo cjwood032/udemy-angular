643eecf3ce98c62c15ef6baba75fb5b4e4c905df
cjwood032 <cjwood032@gmail.com>
2019-08-11 03:36:00 -0400

    ngClass completed

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,7 +1,12 @@
 import {Component} from '@angular/core';
+
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html'
+    //styles: [ `
+    //online {
+    //    background-color: light-green;
+    //}`]
 })
 export class ServerComponent {
     serverID: number = 10; // you can assign types as so.
